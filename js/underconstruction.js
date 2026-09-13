7cbbc3b3224a9e5704ac08863a26ef3b2d0fa61b
hi-its-matte <139715336+hi-its-matte@users.noreply.github.com>
2026-09-13 16:45:23 +0200

    General Update

---
+++ js/underconstruction.js
@@ -0,0 +1,7 @@
+
+    // Imposta su true quando vuoi attivare la pagina di manutenzione
+    const inManutenzione = true;
+
+    if (inManutenzione) {
+        window.location.href = "/pages/underconstruction.html";
+    }
